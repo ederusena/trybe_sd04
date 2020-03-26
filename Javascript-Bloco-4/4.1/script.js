8d577af5c29ae58511f01d88e8f3d104cc89bd09
ederusena <eder.sena@live.com>
2020-03-26 15:45:20 -0300

    Javascript Tipo Primitivos

--- Javascript-Bloco-4/4.1/script.js
+++ Javascript-Bloco-4/4.1/script.js
@@ -1,3 +1,21 @@
-let idade = 5
+//let idade = 5
+//console.log(idade)
 
-alert(idade)
+/* Tipagem */
+// let nome = 'Rafael' //string literal
+// let idade = 25 //number literal
+// let estaAprovado = true //boolean
+// let sobreNome = undefined // Undefined
+// let corSelecionado = null // sem valor
+
+// typeof nome
+
+// +, -, *, /, **
+console.log(salario + salario);
+console.log(salario - salario);
+console.log(salario * salario);
+console.log(salario / salario);
+console.log(salario ** salario);
+
+let idade = 18
+console.log(idade=++);
