e47d2129d7227bdc6a14c9514ec19ad8bf8aafce
ederusena <eder.sena@live.com>
2020-03-26 17:17:40 -0300

    Operador logico

--- Javascript-Bloco-4/4.1/script.js
+++ Javascript-Bloco-4/4.1/script.js
@@ -10,12 +10,32 @@
 
 // typeof nome
 
-// +, -, *, /, **
+// +, -, *, /, **q
+let salario = 10;
 console.log(salario + salario);
 console.log(salario - salario);
 console.log(salario * salario);
 console.log(salario / salario);
 console.log(salario ** salario);
+console.log(salario % salario);
 
 let idade = 18
-console.log(idade=++);
+console.log(idade++);
+
+// operadores de atribuição e operadores lógicos.
+let valorTeclado = 100;
+valorTeclado += valorTeclado;
+console.log(valorTeclado)
+
+valorTeclado = 100;
+valorTeclado -= valorTeclado;
+console.log(valorTeclado)
+
+// Operador logico &&
+// Retorna true para quando os valores derem verdadeiro para a comparacao
+
+let num1 = 10;
+let num2 = 10;
+let bool = num1 && num2;
+console.log( bool );
+
