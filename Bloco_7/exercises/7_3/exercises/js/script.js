3aa8fed95e544352c2f0b02b72be485701ca1760
Eder Sena <eder.sena@live.com>
2020-04-29 11:09:29 -0300

    Correções de boas praticas

--- Bloco_7/exercises/7_3/exercises/js/script.js
+++ Bloco_7/exercises/7_3/exercises/js/script.js
@@ -1,41 +1,39 @@
-window.onload = () => {
-    var clickCard = document.getElementsByClassName("turned-img");
+window.onload = ((doc) => {
+    let $clickCard = doc.getElementsByClassName("turned-img");
 
-    clickCard[0].addEventListener('click', function () {
+    $clickCard[0].addEventListener('click', function() {
 
-        var countCard = document.getElementsByClassName("card");
-        var div = document.createElement("div");                   // Create a <div> element
-        var imgElement = document.createElement("img");
-        var divContainer = document.getElementsByClassName("turned-card"); // Get Element div container for add other
-        var img = `./img/${getRandomImage()}`;      // adicionar path da imagem randomicamente
-        imgElement.src = img;                                   // insert imabe
-        imgElement.classList.add(`${getRandomTransform()}`);
-        div.classList.add("card");
-        div.appendChild(imgElement);
-        if (countCard.length <= 6) {
-            divContainer[0].appendChild(div);                             // Append <p> to <body>
-        } else if (countCard.length <= 13) {
-            divContainer[1].appendChild(div);
-        }
+        let $countCard = doc.getElementsByClassName("card");
+        let $div = doc.createElement("div"); // Create a <div> element
+        let $imgElement = doc.createElement("img");
+        let $divContainer = doc.getElementsByClassName("turned-card"); // Get Element div container for add other
+        let img = `./img/${getRandomImage()}`; // adicionar path da imagem randomicamente
+
+        $imgElement.src = img; // insert imabe
+        $imgElement.classList.add(`${getRandomTransform()}`);
+        $div.classList.add("card");
+        $div.appendChild($imgElement);
+
+        if ($countCard.length <= 6)
+            $divContainer[0].appendChild($div); // Append <p> to <body>
+        else if ($countCard.length <= 13)
+            $divContainer[1].appendChild($div);
+
     });
 
-    // Função que retorna uma imagem aleatoria 
+    // Função que retorna uma imagem aleatoria
     function getRandomImage() {
-    var arrImage = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"];
-    // Calculo randomico para retornar um index aleatorio do nosso array de classes.
-    // Pegamos um valor randomico decimal de 0 a 1 e multiplicamos pelo tamanho do array e arredondamos para baixo.
-    //  ex: 0.8420929154329106 * 7 = 5.894650408030374 -> 5
-    const imageIndex = Math.floor(Math.random() * arrImage.length)
-    return arrImage[imageIndex]; // Em seguida retornamos o item que está nessa posição aleatoria do array
-}
- // Função que retorna uma classe aleatoria de transformacao
-function getRandomTransform() {
-    var transforms = [
-        "rotate", "translate-up", "translate-down",
-        "scale-up", "scale-down", "skew-up", "skew-down"
-      ]
-    const transformIndex = Math.floor(Math.random() * transforms.length)
-    return transforms[transformIndex]; // Em seguida retornamos o item que está nessa posição aleatoria do array
-  }
-}
-
+        let arrImage = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"];
+        const imageIndex = Math.floor(Math.random() * arrImage.length)
+        return arrImage[imageIndex]; // Em seguida retornamos o item que está nessa posição aleatoria do array
+    }
+    // Função que retorna uma classe aleatoria de transformacao
+    function getRandomTransform() {
+        let transforms = [
+            "rotate", "translate-up", "translate-down",
+            "scale-up", "scale-down", "skew-up", "skew-down"
+        ]
+        const transformIndex = Math.floor(Math.random() * transforms.length)
+        return transforms[transformIndex]; // Em seguida retornamos o item que está nessa posição aleatoria do array
+    }
+})(document);
