3cf04b484d7c4a54b7f544bfd9cd7a8fe201bc1d
Eder Sena <eder.sena@live.com>
2020-05-04 14:58:52 -0300

    Parte 2 - Object.keys

--- Bloco_8/exercises/8_2/conteudo.js
+++ Bloco_8/exercises/8_2/conteudo.js
@@ -1,17 +1,31 @@
-function fabricarPessoa(nome, sobrenome){
+function fabricarPessoa(nome, sobrenome, age, job){
     let pessoa = {};
     pessoa.nome = nome;
     pessoa.sobrenome = sobrenome;
+    pessoa.age = age;
+    pessoa.job = job;
 
     return pessoa;
 }
 
+// Parte 1 - Adicionando novas chaves
 let pessoaA = fabricarPessoa('Eder', 'Sena');
 let pessoaB = fabricarPessoa('Damaris', 'Monteiro');
 
-// Parte 1 - Adicionando novas chaves
 pessoaA.age = "32";
 pessoaA['sobrenome'] = "Soares";
 
 console.log(pessoaA, pessoaB);
+
+// Parte 2 - Object.keys
+
+let customer = fabricarPessoa('Roberto', undefined, 22, 'Teacher');
+let newKeys = 'lastName';
+const lastName = 'Ferreira';
+customer[newKeys] = lastName;
+newKeys = 'fullname';
+const fullname = `${customer.nome} ${lastName}`
+customer[newKeys] = fullname;
+
+console.log(customer)
 
