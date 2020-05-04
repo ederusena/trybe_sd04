704fc5801826acd7243efc4be7ebb6b0042f78db
Eder Sena <eder.sena@live.com>
2020-05-04 15:34:49 -0300

    Parte 2 - Object.keys - Ex

--- Bloco_8/exercises/8_2/conteudo.js
+++ Bloco_8/exercises/8_2/conteudo.js
@@ -17,8 +17,6 @@ pessoaA['sobrenome'] = "Soares";
 
 console.log(pessoaA, pessoaB);
 
-// Parte 2 - Object.keys
-
 let customer = fabricarPessoa('Roberto', undefined, 22, 'Teacher');
 let newKeys = 'lastName';
 const lastName = 'Ferreira';
@@ -29,3 +27,40 @@ customer[newKeys] = fullname;
 
 console.log(customer)
 
+// Parte 2 - Object.keysssss
+
+console.log(Object.keys(customer))
+
+// getFoo é uma propriedade que não é enumerável
+var my_obj = Object.create({}, { getFoo: { value: () =>{ return foo; } } });
+my_obj.foo = 1;
+my_obj.carro = {'modelo': 'corsa', 'cor': 'verde'}
+console.log(Object.keys(my_obj)); // console: ['foo']
+
+const student1 = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkills: 'Ótimo',
+  };
+  
+  const student2 = {
+    Html: 'Bom',
+    Css: 'Ótimo',
+    JavaScript: 'Ruim',
+    SoftSkills: 'Ótimo',
+    Git: 'Bom', // chave adicionada
+  };
+
+  const listSkills = (student) => {
+    const arrayOfSkills = Object.keys(student);
+    for(i in arrayOfSkills){
+      console.log(`${arrayOfSkills[i]}, Nível: ${student[arrayOfSkills[i]]}`);
+    }
+  };
+
+  console.log('Estudante 1');
+  listSkills(student1);
+
+  console.log('Estudante 2');
+  listSkills(student2);
