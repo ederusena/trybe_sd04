822baa22e39b19d0e27c86b20c58d4f819e2c9d5
Eder Sena <eder.sena@live.com>
2020-05-04 15:39:47 -0300

    Parte 3- Objet.values

--- Bloco_8/exercises/8_2/conteudo.js
+++ Bloco_8/exercises/8_2/conteudo.js
@@ -64,3 +64,28 @@ const student1 = {
 
   console.log('Estudante 2');
   listSkills(student2);
+
+//   Parte III - Object.values
+const student = {
+    Html: 'Muito Bom',
+    Css: 'Bom',
+    JavaScript: 'Ótimo',
+    SoftSkill: 'Ótimo',
+  };
+  
+  const listSkillsWithFor = (student) => {
+    const skills = [];
+    for(skill in student) {
+      skills.push(student[skill]);
+    }
+  
+    return skills;
+  };
+  
+  const listSkillsWithValues = (student) => Object.values(student);
+  
+  // Sem Object.values
+  console.log(listSkillsWithFor(student));
+  
+  // Com Object.values
+  console.log(listSkillsWithValues(student));
