b38be71ddd9eaf5c21d1322ae4e2def13e8f234d
Eder Sena <eder.sena@live.com>
2020-05-05 18:35:22 -0300

    Exercicios 8.3 - 1-4

--- Bloco_8/exercises/8_3/exercicio1.js
+++ Bloco_8/exercises/8_3/exercicio1.js
@@ -16,5 +16,7 @@ assert.equal(sum(4,5),9)
 assert.equal(sum(0,0),0)
 
 // Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
-assert.equal(sum(4,5),9)
+// assert.equal(sum(4,"5"),9)
+
 // Teste se a mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5")
+assert.throws(() => { (sum(4,"5"),9); }, /^Error: parameter y must not be Strings$/); // OK
