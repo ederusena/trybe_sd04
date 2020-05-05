1cb70cfb5aa9c88a3f03d2988661cb368bb4c6be
Eder Sena <eder.sena@live.com>
2020-05-05 18:57:46 -0300

    Exercicios 8.3 - 4.4

--- Bloco_8/exercises/8_3/exercicio4.js
+++ Bloco_8/exercises/8_3/exercicio4.js
@@ -20,7 +20,8 @@ function myRemove(arr, item) {
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4])
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-
+assert.notDeepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4])
 // Verifique se o array passado por parâmetro não sofreu alterações
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4])
