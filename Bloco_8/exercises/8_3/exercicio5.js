e32c2882766cb1587a8ac7e7aa1b583e069da56d
Eder Sena <eder.sena@live.com>
2020-05-05 19:03:45 -0300

    Exercicios 8.3 - 5.2

--- Bloco_8/exercises/8_3/exercicio5.js
+++ Bloco_8/exercises/8_3/exercicio5.js
@@ -19,7 +19,9 @@ function myRemoveWithoutCopy(arr, item) {
 
 // 1. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado
 assert.deepEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4])
+
 // 2. Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4])
 
 // 3. Faça uma chamada para a função myRemoveWithoutCopy e verifique se o array passado por parâmetro sofreu alterações
 
