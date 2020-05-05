966d845f8950298f9d205c94f60a639ba8397a93
Eder Sena <eder.sena@live.com>
2020-05-05 14:23:44 -0300

    Parte 2.3 - Func Show length

--- Bloco_8/exercises/8_2/parte2.js
+++ Bloco_8/exercises/8_2/parte2.js
@@ -18,11 +18,15 @@ const lesson1 = {
     turno: 'noite',
   };
 
-// Exercicio 1 parte 1
+// Ex 1.1 Crie uma função para adicionar o turno da manhã na lesson2. Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
 const ex1 = (obj, key, value) => obj[key] = value;
 console.log(ex1(lesson2, 'turno', 'manhã'));
 console.log(lesson2);
 
-// Exercicio 1 parte 2 
+// Ex 1.2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 const ex2 = obj => Object.keys(obj);
 console.log(ex2(lesson2));
+
+// Ex 1.3 Crie uma função para mostrar o tamanho de um objeto.
+const ex3 = obj => Object.keys(obj).length;
+console.log(ex3(lesson2));
