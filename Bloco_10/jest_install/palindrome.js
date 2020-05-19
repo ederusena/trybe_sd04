495d44c025e0a1b886d96eb393ad6bec50cce89a
Eder Sena <eder.sena@live.com>
2020-05-19 03:17:03 -0300

    palindrome

---
+++ Bloco_10/jest_install/palindrome.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+// function palindrome(str) {
+//   let re = /[\W_]/g;
+//   let lowRegStr = str.toLowerCase().replace(re, '');
+//   let reverseStr = lowRegStr.split('').reverse().join(''); 
+//   return reverseStr === lowRegStr;
+// }
+
+function palindrome(str) {
+  var re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  var len = str.length;
+  for (var i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+        return false;
+    }
+  }
+  return true;
+ }
+module.exports = palindrome;
