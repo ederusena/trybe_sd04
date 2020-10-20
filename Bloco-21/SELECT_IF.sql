8c5e6d622053d2c1896d51b3be39c5193fb71c27
Eder Sena <eder.sena@live.com>
2020-10-20 17:00:28 -0200

    Update SELECT_IF.sql

--- Bloco-21/SELECT_IF.sql
+++ Bloco-21/SELECT_IF.sql
@@ -6,3 +6,15 @@ case
     else 'Duração Longa'
 end as type_duration
 from film;
+
+SELECT length('Quase sextou bb'); -- exibe tamanho da string passada como parametro
+select UCASE('burgao'); -- transforma em caixa alta a string passada como parametro
+select LCASE('BURGAOOO');  -- transforma em caixa baixa a string passada como parametro
+select LEFT('COD-1234556-MG', 2); -- extrai 2 caracteres a partir da esquerda
+select RIGHT('COD-1234556-MG', 2); -- extrai 2 caracteres a partir da right
+select substring('tenhofomedemais', 5); -- extrai 2 caracteres a partir do 5 caractere
+
+-- 1 parametro -> posicao inicial de extracao
+-- 2 parametro -> tamanho de caracteres que quero extrair
+
+select substring('tenhofomedemais', 6,4); 
