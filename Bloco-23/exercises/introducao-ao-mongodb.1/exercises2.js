e2c31dcd15d0b8d430e7235e4b441cad9bbff645
Eder Sena <eder.sena@live.com>
2020-11-06 20:26:51 -0300

    exercicio 19

--- Bloco-23/exercises/introducao-ao-mongodb.1/exercises2.js
+++ Bloco-23/exercises/introducao-ao-mongodb.1/exercises2.js
@@ -67,12 +67,38 @@ db.superheroes.find( { $and: [{ $or: [{ "aspects.height": 180 }, { "aspects.heig
 
 // Exercício 15: Selecione todos os super-heróis que pesem entre 80kg e 100kg
 // sejam Humanos ou Mutantes e não sejam publicados pela DC Comics.
-
+db.superheroes.find(
+    {
+        $and: [
+            {
+                "aspects.weight": { $gte: 80, $lte: 100 }
+            },
+            {
+                $or: [{ race: "Human" }, { race: "Mutant" }]
+            },
+            {
+                publisher: { $ne: "DC Comics" }
+            }
+        ]
+    }
+).pretty();
 
 // Exercício 16: Retorne o total de documentos que não contêm o campo race.
+db.superheroes.count(
+    { race: { $exists: false } }
+);
 
 // Exercício 17: Retorne o total de documentos que contêm o campo hairColor.
+db.superheroes.count(
+    { "aspects.hairColor": { $exists: true } }
+);
 
 // Exercício 18: Remova apenas um documento publicado pela Sony Pictures.
+db.superheroes.deleteOne(
+    { publisher: "Sony Pictures" }
+);
 
 // Exercício 19: Remova todos os documentos publicados pelo George Lucas.
+db.superheroes.deleteMany(
+    { publisher: "George Lucas" }
+);
