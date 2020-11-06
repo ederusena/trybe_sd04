4da780b8be04a27c5cd62d78f432b180c520999f
Eder Sena <eder.sena@live.com>
2020-11-06 19:15:27 -0300

    exercicio 10

--- Bloco-23/exercises/introducao-ao-mongodb.1/exercises2.js
+++ Bloco-23/exercises/introducao-ao-mongodb.1/exercises2.js
@@ -1,21 +1,45 @@
 // Exercício 1: Inspecione um documento para que você se familiarize com eles.
 // Entenda os campos e os níveis existentes no documento escolhido.
+// git add .; git commit -m "exercicio 1". git push -u origin exercises/introducao-ao-mongodb.1
+db.superheroes.find({}).limit(1).pretty();
+{
+	"_id" : ObjectId("5fa5b476dcfc9973be0a4134"),
+	"name" : "Abomination",
+	"alignment" : "bad",
+	"gender" : "Male",
+	"race" : "Human / Radiation",
+	"aspects" : {
+		"eyeColor" : "green",
+		"hairColor" : "No Hair",
+		"height" : 203,
+		"weight" : 200
+	},
+	"publisher" : "Marvel Comics"
+}
 
 // Exercício 2: Selecione todos os super-heróis com menos de 1.80m de altura. Lembre-se de que essa informação está gravada em centímetros.
+db.superheroes.find({ "aspects.height": { $lt: 180}});
 
 // Exercício 3: Retorne o total de super-heróis menores que 1.80m.
+db.superheroes.find({ "aspects.height": { $lt: 180}}).count();
 
 // Exercício 4: Retorne o total de super-heróis com até 1.80m.
+db.superheroes.find({ "aspects.height": { $lte: 180}}).count();
 
 // Exercício 5: Selecione um super-herói com 2.00m ou mais de altura.
+db.superheroes.find({ "aspects.height": { $gte: 200}});
 
 // Exercício 6: Retorne o total de super-heróis com 2.00m ou mais.
+db.superheroes.find({ "aspects.height": { $gte: 200}}).count();
 
 // Exercício 7: Selecione todos os super-heróis que têm olhos verdes.
+db.superheroes.find({ "aspects.height": { $gte: 200}}).count();
 
 // Exercício 8: Retorne o total de super-heróis com olhos azuis.
+db.superheroes.find({ "aspects.eyeColor": "blue" }).count();
 
-// Exercício 9: Utilizando o operador $in, selecione todos os super-heróis com cabelos pretos ou carecas ("No Hair").
+// Exercício 9: Utilizando o operador $in, 
+// selecione todos os super-heróis com cabelos pretos ou carecas ("No Hair").
 
 // Exercício 10: Retorne o total de super-heróis com cabelos pretos ou carecas ("No Hair").
 
